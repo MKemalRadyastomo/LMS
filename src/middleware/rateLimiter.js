ba29796859c4e9f3ed8bd6656cd002ebc499a716
M Kemal R <mkradyastomo@gmail.com>
2025-05-26 17:54:23 +0700

    [Kemal] Ganti limiter

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -22,7 +22,7 @@ const apiLimiter = rateLimit({
  */
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // limit each IP to 10 requests per windowMs
+  max: 99999, // limit each IP to 10 requests per windowMs
   standardHeaders: true,
   legacyHeaders: false,
   handler: (req, res) => {
